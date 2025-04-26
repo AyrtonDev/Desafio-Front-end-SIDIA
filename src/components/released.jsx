a6953439ad2043731290dee030d0aac14aa8a84b
Luan Cruz <72052905+Luan-Cruz@users.noreply.github.com>
2025-04-26 19:39:55 -0400

    projetobase

--- src/components/released.jsx
+++ src/components/released.jsx
@@ -6,9 +6,8 @@ export function Released ({movies}){
                 {movies.map((movie)=>(
                     <div className=" flex flex-col items-center gap-6">
                         <div>
-
-                        <img src={"https://image.tmdb.org/t/p/w300" + movie.backdrop_path} alt="post movie" className="w-full h-50 object-cover rounded-lg"/>
-                        </div>
+                            <img src={"https://image.tmdb.org/t/p/w300" + movie.backdrop_path} alt="post movie" className="w-full h-50 object-cover rounded-lg"/>
+                            </div>
                         <span className="text-white font-semibold text-center text-lg">{movie.title}</span>
                     </div>
                 ))}
